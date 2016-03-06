0303518c48a9791984099bad71b446ec5f76de32
Jerry Li <jerryli97@outlook.com>
2016-03-06 00:48:13 -0500

    Implemented tracking of current video time

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
-	res.render('index', {title: 'Video'})
+	res.render('watch', {title: 'Video'})
 })
 //<<<<<<< HEAD
 /* GET Hello World page. 
